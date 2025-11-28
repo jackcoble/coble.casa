6b24737692dad254976759748617661bb959b4c6
Jack Coble <jack@coble.uk>
2025-11-28 13:36:19 +0000

    feat(infra): rocky linux 10 cloud image resource

--- opentofu/main.tf
+++ opentofu/main.tf
@@ -1,3 +1,10 @@
-output "complex-yaml" {
-    value = provider::sops::file("../secrets/secrets.yaml")
+# Rocky Linux 10.1 Cloud Image
+resource "proxmox_virtual_environment_download_file" "rocky_linux_10_1_cloud_image" {
+  content_type       = "import"
+  datastore_id       = "local"
+  file_name          = "rocky-linux-10-generic-cloud-base.qcow2"
+  node_name          = "pve"
+  url                = "https://dl.rockylinux.org/pub/rocky/10/images/x86_64/Rocky-10-GenericCloud-Base.latest.x86_64.qcow2"
+  checksum           = "28628abf08a134c6f9e1eccbcac3f2898715919a6da294ae2c6cd66d6bc347ad"
+  checksum_algorithm = "sha256"
 }
